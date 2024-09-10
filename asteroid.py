4486ac34087fee170e15668bb074191da30797de
ichiz7 <ichibakry7@gmail.com>
2024-09-10 16:08:21 +0200

    add asteroid split and kill logic

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import *
 
 
 class Asteroid(CircleShape):
@@ -12,3 +14,16 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        rand_angle = random.uniform(20, 50)
+        vecotr_1 = self.position.rotate(rand_angle)
+        vecotr_2 = self.position.rotate(-rand_angle)
+        self.radius -= ASTEROID_MIN_RADIUS
+        asteroid_1 = Asteroid(self.position.x, self.position.y, self.radius)
+        asteroid_1.velocity = vecotr_1 * 1.2
+        asteroid_2 = Asteroid(self.position.x, self.position.y, self.radius)
+        asteroid_2.velocity = vecotr_2
--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.collision_check(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         pygame.display.flip()
